010680b29a1553575532632946c17c446f4c7c23
Eozef <eozefow@gmail.com>
2021-01-05 22:50:05 +1100

    Adding Links in the Navbar, add react router bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 function App() {
   return (
     <div className="App container py-3">
-      <Navbar collapseOnSelect bg="dark" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          Note
-        </Navbar.Brand>
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Note
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end ">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link href="/signup">Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link href="/login">Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
